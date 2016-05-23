d58285353908621829aa443b76ca9b5d5195f3b8
Eduardo Veras <eduardo.storm@gmail.com>
2016-05-22 20:48:25 -0400

    Final changes and optimzation

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -13,16 +13,16 @@ import java.util.Scanner;
  */
 public class Main {
 
-    private static String URL = "http://www.fb.com";
-    private static String Body = null;
+    private static String URL =null;
 
     public static void main(String[] args) {
-        getUrl();
-        getData();
-
+        if (getUrl())
+        {
+            getData();
+        }
     }
 
-    public static void getUrl(){
+    private static boolean getUrl(){
         Scanner sc = new Scanner(System.in);
         URL = sc.nextLine().trim();
         System.out.println("The url you typed is: "+ URL);
@@ -30,10 +30,12 @@ public class Main {
         UrlValidator validator = new UrlValidator(schemes);
         if (validator.isValid(URL )) {
             System.out.println("url is valid");
+            return true;
         }
         else {
-            System.out.println("url is invalid");
+            System.out.println("url is invalid....exiting now");
         }
+        return false;
     }
 
     private static void getData(){
@@ -43,18 +45,16 @@ public class Main {
         } catch (IOException e) {
             //e.printStackTrace();
             System.out.println("There was an error accesing the URL,Try with another one");
+            System.exit(-1);
         }
-        Elements elementIMG = docu.getElementsByTag("img");
-        Elements elementParagraph = docu.getElementsByTag("p");
         Elements elementForms = docu.getElementsByTag("form");
-        if (docu!=null){
             System.out.println("This is the title: '"+ docu.title()+"'");
             System.out.println("This is the adress: " +docu.location());
             System.out.println("This is the Node Name: " +docu.nodeName());
             System.out.println("This is the line count: " + countLinesBody(docu.html()));
-            System.out.println("This is the Paragraph Count: "+ elementParagraph.size());
-            System.out.println("This is the Images Count: "+ elementIMG.size());
-            System.out.println("This is the Form Count: "+ elementForms.size());
+            System.out.println("This is the Paragraph Count: "+ docu.getElementsByTag("p").size());
+            System.out.println("This is the Images Count: "+ docu.getElementsByTag("img").size());
+            System.out.println("This is the Form Count: "+ docu.getElementsByTag("form").size());
             System.out.println();
             System.out.println("\tProcesing forms....\n");
             for (int i = 0; i < elementForms.size(); i++) {
@@ -65,24 +65,16 @@ public class Main {
                             ""+(i+1));
                     List AttrList = inputlist.get(j).attributes().asList();
                     System.out.println("\t\t\tElement Attributes:");
-                    for (int k = 0; k < AttrList.size(); k++) {
-                        System.out.println("\t\t\t\t" + AttrList.get(k));
+                    for (Object aAttrList : AttrList) {
+                        System.out.println("\t\t\t\t" + aAttrList);
                     }
-
-
-
                 }
-
             }
-            //System.out.println(docu.html());
-
-        }
 
     }
     private static int countLinesBody(String str){
         String[] lines = str.split("\n" );
         return  lines.length;
-
 
     }
 
